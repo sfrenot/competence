01054489aeb8fcef76d006d588a67355f013c2d3
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2018-06-22 15:47:37 +0200

    Ajout du code de tp

---
+++ ELK/cleanElastic.sh
@@ -0,0 +1,6 @@
+curl -XDELETE tc405-112-01:9200/competences.enseignants
+curl -XDELETE tc405-112-01:9200/competences.ues
+curl -XDELETE tc405-112-01:9200/competences.vocabulaires
+curl -XDELETE tc405-112-01:9200/competences.semestres
+curl -XDELETE tc405-112-01:9200/competences.ecs
+curl -XDELETE tc405-112-01:9200/competences.niveaucompetences
