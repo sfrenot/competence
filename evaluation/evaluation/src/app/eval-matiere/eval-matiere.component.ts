f31e4b6f5a0f882b43582050ea781bfc1a225dcf
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2019-09-19 11:36:28 +0200

    Ajout composant evaluation matiere

---
+++ evaluation/evaluation/src/app/eval-matiere/eval-matiere.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Matiere } from '../matiere'
+
+@Component({
+  selector: 'app-eval-matiere',
+  templateUrl: './eval-matiere.component.html',
+  styleUrls: ['./eval-matiere.component.css']
+})
+export class EvalMatiereComponent implements OnInit {
+  @Input() evalMatiere: Matiere;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
